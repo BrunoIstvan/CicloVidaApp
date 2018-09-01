a89fa3dd9431aece670401b77df398e2f2d81e73
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2018-09-01 15:22:29 -0300

    Initial commit

---
+++ app/src/main/java/br/com/bicmsystems/ciclodevida/MainActivity.kt
@@ -0,0 +1,21 @@
+package br.com.bicmsystems.ciclodevida
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+
+        super.onSaveInstanceState(outState)
+
+
+
+    }
+
+}
